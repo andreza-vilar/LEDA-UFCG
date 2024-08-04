34ecbb7fc9e0ee26e0c2680d7df2b3a9589cd72e
andreza vilar <andreza.farias@ccc.ufcg.edu.br>
2024-08-04 10:29:49 -0300

    add: Questões TST-EDA

---
+++ TST-EDA/InsercaoOrdenadaLast.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] inputArray = input.split(" ");
+        int[] arr = new int[inputArray.length];
+
+        // Convertendo a entrada para um array de inteiros
+        for (int i = 0; i < inputArray.length; i++) {
+            arr[i] = Integer.parseInt(inputArray[i]);
+        }
+
+        // Reordenando o último elemento
+        int n = arr.length;
+        int lastElement = arr[n - 1];
+        int i = n - 2;
+
+        // Movendo elementos maiores que o último elemento uma posição para a frente
+        while (i >= 0 && arr[i] > lastElement) {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+        // Colocando o último elemento na posição correta
+        arr[i + 1] = lastElement;
+
+        // Imprimindo o array ordenado
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
